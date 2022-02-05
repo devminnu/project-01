e934080727c7ed7fbda785992b2359a808b51823
Minhajuddin Quazi <minhajqazi@live.com>
2022-02-05 20:51:49 +0530

    first commit

---
+++ commands.sh
@@ -0,0 +1,11 @@
+# initialize empty git repo
+git init
+
+# set default branch name
+ git config --global init.defaultBranch main
+
+#  change branch name from master to main
+git branch -m main
+
+# git status
+git status
