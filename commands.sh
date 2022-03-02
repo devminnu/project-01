77b3bb7b78709bea9411ad4033fe15f344128788
Minhajuddin Quazi <minhajqazi@live.com>
2022-03-02 14:28:06 +0530

    updated command

--- commands.sh
+++ commands.sh
@@ -1,11 +1,22 @@
 # initialize empty git repo
 git init
 
-# set default branch name
+# set default branch name on every new repo initialization with git init command
+ git config --global init.defaultBranch BRANCH_NAME
  git config --global init.defaultBranch main
 
-#  change branch name from master to main
+#  change/rename branch name
+git branch -m BRANCH_NAME
 git branch -m main
 
 # git status
 git status
+
+# to stage files
+git add FILE_1 FILE_2
+
+# to unstage files
+git rm --cached FILE_1 FILE_2
+
+# git restore - restores changes to a file
+git restore FILE_NAME
--- main.go
+++ main.go
@@ -1,5 +1,75 @@
 package main
 
-func main() {
+import (
+	"context"
+	"errors"
+	"fmt"
+	"sync"
+	"time"
+)
 
+func main() {
+	ctx, _ := context.WithCancel(context.Background())
+	Run(ctx, t)
+	for err := range Run(ctx, t) {
+		fmt.Println("Err:", err)
+		// cancel()
+	}
+	// Run(func() error {
+	// 	if time.Now().Second()%2 == 0 {
+	// 		return errors.New("2 test error")
+	// 	}
+	// 	return nil
+	// },
+	// 	func() error {
+	// 		if time.Now().Second()%3 == 0 {
+	// 			return errors.New("3 test error")
+	// 		}
+	// 		return nil
+	// 	},
+	// 	func() error {
+	// 		if time.Now().Second()%4 == 0 {
+	// 			return errors.New("4 test error")
+	// 		}
+	// 		return nil
+	// 	})
+}
+
+// func Run(tasks ...func() error) (err error) {
+// 	g := new(errgroup.Group)
+// 	for _, t := range tasks {
+// 		g.Go(t)
+// 	}
+// 	// Wait for all go routines to complete.
+// 	if err = g.Wait(); err != nil {
+// 		log.Error(err)
+
+// 		return
+// 	}
+// 	log.Info("No errors occurred")
+
+// 	return
+// }
+
+func Run(ctx context.Context, tasks ...func(context.Context, *sync.WaitGroup, chan<- error)) (errCh chan error) {
+	n := len(tasks)
+	errCh = make(chan error, n)
+
+	w := &sync.WaitGroup{}
+	w.Add(n)
+
+	for _, t := range tasks {
+		go t(ctx, w, errCh)
+	}
+
+	w.Wait()
+	close(errCh)
+
+	return
+}
+
+func t(ctx context.Context, wg *sync.WaitGroup, errChan chan<- error) {
+	if time.Now().Second()%2 == 0 {
+		errChan <- errors.New("2 test error")
+	}
 }
